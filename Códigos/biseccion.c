f6fb020fd4b7201a4fc734a3b78e0611aac82577
Marcos Agustin Garcia Mombello <mgarcia975@alumnos.iua.edu.ar>
2024-08-14 09:33:41 -0300

    Códigos Biseccion y Regula Falsi

---
+++ Códigos/biseccion.c
@@ -0,0 +1,83 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <string.h>
+float f(float);
+
+int main(int argc, char *argv[])
+{
+float a = 0;
+float b = 0;
+float c = 0;
+float tolerancia;
+float error;
+float Xr;
+int errAbsExact;
+int errAbsAprox;
+int errPorcExact;
+int errPorcAprox;
+int opcion;
+
+printf("Introduzca 'a': \n");
+scanf("%f", &a);
+printf("Introduzca 'b': \n");
+scanf("%f", &b);
+printf("Verificando si es posible calcular la raíz de la función.\n");
+if(f(a)*f(b) < 0)
+{
+    printf("Es posible calcular la raíz.\n");
+}
+else
+{
+    printf("No es posible calcular la raíz.\n");
+    exit(1);
+}
+printf("Introduzca la tolerancia: \n");
+scanf("%f",&tolerancia);
+//printf("Elija su tipo de error:\n 1- Error absoluto exacto.\n 2- Error absoluto aproximado.\n 3- Error porcentual exacto.\n 4- Error porcentual aproximado.");
+/*switch(opcion)
+{
+    case 1:errAbsExact = 1;
+    break; 
+    case 2:errAbsAprox = 1;
+    break;
+    case 3:errPorcExact = 1;
+    break;
+    case 4:errPorcAprox = 1;
+    break;
+};*/
+
+
+
+do
+{
+    c = (a+b)/2;
+    if((f(a)*f(c)) > 0)
+    {
+        a = c;
+    }
+    else
+    {
+        if((f(a)*f(c)) < 0)
+        {
+            b = c;
+        }
+        else
+        {
+            printf("Raíz exacta encontrada: %f", c);
+            exit(0);
+        }
+    }
+    error = (b-a)/2;
+} while (error > tolerancia);
+printf("Raíz: %f +- %f", c,error);
+    return 0;
+    }
+
+
+float f (float x)
+{
+    float r = 0; 
+    r = -2 + 7*x -5 * pow(x,2) + 6 * pow(x,3);
+    return r;
+};
---
+++ Códigos/regulaFalsi.c
@@ -0,0 +1,64 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <string.h>
+float f(float);
+
+int main(int argc, char *argv[])
+{
+float a = 0;
+float b = 0;
+float c = 0;
+float tolerancia;
+float error;
+float Xr;
+
+printf("Introduzca 'a': \n");
+scanf("%f", &a);
+printf("Introduzca 'b': \n");
+scanf("%f", &b);
+printf("Verificando si es posible calcular la raíz de la función.\n");
+if(f(a)*f(b) < 0)
+{
+    printf("Es posible calcular la raíz.\n");
+}
+else
+{
+    printf("No es posible calcular la raíz.\n");
+    exit(1);
+}
+printf("Introduzca la tolerancia: \n");
+scanf("%f",&tolerancia);
+
+do
+{
+    c = ((a*f(b))-(b*f(a)))/(f(b)-f(a));
+    if((f(a)*f(c)) > 0)
+    {
+        a = c;
+    }
+    else
+    {
+        if((f(a)*f(c)) < 0)
+        {
+            b = c;
+        }
+        else
+        {
+            printf("Raíz exacta encontrada: %f", c);
+            exit(0);
+        }
+    }
+    error = fabs(b-a)/2;
+} while (error > tolerancia);
+printf("Raíz: %f +- %f", c,error);
+    return 0;
+    }
+
+
+float f (float x)
+{
+    float r = 0; 
+    r = -2 + 7*x -5 * pow(x,2) + 6 * pow(x,3);
+    return r;
+};
